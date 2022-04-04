a4ce25ccbbb57b8cf17a95e274e40dcb5a5cdf9b
Dhencio <denysdon15@gmail.com>
2022-04-04 18:04:28 +0800

    ✨ init project and setup mantine

---
+++ README.md
@@ -0,0 +1,3 @@
+# RECIPE BY edamam
+
+- API https://api.edamam.com/api/recipes/v2
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { Text } from "@mantine/core";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <Text>HELLO</Text>;
+};
+
+export default Home;
